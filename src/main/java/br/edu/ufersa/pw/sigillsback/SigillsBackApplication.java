a11f79a16c6f2559a0d6227b534e7a0593d3f45b
JFelipeBS <f.filipisilva13@hotmail.com>
2022-06-20 18:30:13 -0300

    update

--- src/main/java/br/edu/ufersa/pw/sigillsback/SigillsBackApplication.java
+++ src/main/java/br/edu/ufersa/pw/sigillsback/SigillsBackApplication.java
@@ -10,6 +10,8 @@ import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 //import br.edu.ufersa.pw.sigillsback.entity.User;
 import br.edu.ufersa.pw.sigillsback.repository.UserRepository;
@@ -30,5 +32,16 @@ public class SigillsBackApplication {
 	public ModelMapper mapper() {
 		return new ModelMapper();
 	}
+
+	@Bean                  
+	public WebMvcConfigurer corsConfigurer(){
+	    return new WebMvcConfigurer(){
+	        @Override
+	        public void addCorsMappings(CorsRegistry registry){
+	        	registry.addMapping("/**").allowedOrigins("*","http://localhost:3000");
+	                                                                     
+	        }                                                        
+	    };                                                           
+	}          
 
 }
